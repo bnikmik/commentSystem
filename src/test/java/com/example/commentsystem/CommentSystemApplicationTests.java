a583ab7d29636a95ba3d4921e7c0220b4ea68e82
Nikita Bolshakov <bnikmik@gmail.com>
2023-03-29 14:16:18 +0300

    initial commit

---
+++ src/test/java/com/example/commentsystem/CommentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
